9a6b4a5e42fd04763915aa812b892b5b6e53a166
qyaniwu <qyaniwu@tencent.com>
2025-07-30 12:47:29 +0800

    fix:修改了目录结构

--- src/components/basic/IconGroups.vue
+++ src/components/basic/IconGroups.vue
@@ -1,36 +1,40 @@
 <template>
   <div class="container-flex-start container-wrap">
-    <div v-for="(svgContent, name) in svgMap" :key="name" class="margin-rem container-column container-align-center">
-      <div v-html="svgContent" style="cursor: pointer;"/>
+    <div
+      v-for="(svgContent, name) in svgMap"
+      :key="name"
+      class="margin-rem container-column container-align-center"
+    >
+      <div style="cursor: pointer" v-html="svgContent" />
       <div>{{ name }}</div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue';
+import { onMounted, ref } from "vue";
 
-const props=defineProps({
+const props = defineProps({
   size: {
     type: String,
-    default: '32'
-  }
+    default: "32",
+  },
 });
 
 // 获取所有.svg文件
-const svgFiles = import.meta.glob('../icons/*.svg');
+const svgFiles = import.meta.glob("../../icons/*.svg");
 const svgMap = ref<Record<string, string>>({});
 
 // 动态加载所有.svg文件
 const loadSvgs = async () => {
   for (const path in svgFiles) {
     try {
-      const name = path.split('/').pop()?.replace('.svg', '')|| '';
-      const module = await import(path+'?raw');
+      const name = path.split("/").pop()?.replace(".svg", "") || "";
+      const module = await import(path + "?raw");
 
       svgMap.value[name] = module.default.replace(
-        '<svg',
-        `<svg width="${props.size}" height="${props.size}"`
+        "<svg",
+        `<svg width="${props.size}" height="${props.size}"`,
       );
     } catch (error) {
       console.error(`Failed to load SVG: ${path}`, error);
@@ -43,10 +47,8 @@ onMounted(() => {
 });
 
 defineOptions({
-  name: 'IconGroups'
+  name: "IconGroups",
 });
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
