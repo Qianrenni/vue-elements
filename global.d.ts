2b1fe4267c1dce2f29631d4041fcedae1fbf471d
qyaniwu <qyaniwu@tencent.com>
2025-08-25 10:20:28 +0800

    feat:增加了global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,105 @@
+// global.d.ts
+// 为 Volar 提供全局组件类型提示
+
+import type {
+    QIcon,
+    QMessage,
+    QPagination,
+    QAvatar,
+    QBadge,
+    QCarousel,
+    QCarouselItem,
+    QDemoBlock,
+    QDivider,
+    QMarkdownRender,
+    QMobileFrame,
+    QProgressBar,
+    QRainFigure,
+    QScrollNotice,
+    QFormButton,
+    QFormCheckboxGroup,
+    QFormColorPicker,
+    QFormContainer,
+    QFormDatalist,
+    QFormDatePicker,
+    QFormFileUpload,
+    QFormRadioGroup,
+    QFormRangeSlider,
+    QFormSelect,
+    QFormSwitch,
+    QFormTable,
+    QFormText,
+    QFormTextarea,
+    QSearch,
+    QCard,
+    QCollapse,
+    QCollapseItem,
+    QCollapsibleSection,
+    QDialog,
+    QDrawer,
+    QScrollContainer,
+    QTree,
+    QTreeNode,
+    QNavSection,
+    QTab,
+    QConditionSelect,
+    QThemeToggle,
+} from './dist/types/index'  // 👈 确保这个路径能正确导入类型
+
+import type { Plugin } from 'vue'
+
+declare module 'vue' {
+    export interface GlobalComponents {
+        QIcon: typeof QIcon
+        QMessage: typeof QMessage
+        QPagination: typeof QPagination
+        QAvatar: typeof QAvatar
+        QBadge: typeof QBadge
+        QCarousel: typeof QCarousel
+        QCarouselItem: typeof QCarouselItem
+        QDemoBlock: typeof QDemoBlock
+        QDivider: typeof QDivider
+        QMarkdownRender: typeof QMarkdownRender
+        QMobileFrame: typeof QMobileFrame
+        QProgressBar: typeof QProgressBar
+        QRainFigure: typeof QRainFigure
+        QScrollNotice: typeof QScrollNotice
+        QFormButton: typeof QFormButton
+        QFormCheckboxGroup: typeof QFormCheckboxGroup
+        QFormColorPicker: typeof QFormColorPicker
+        QFormContainer: typeof QFormContainer
+        QFormDatalist: typeof QFormDatalist
+        QFormDatePicker: typeof QFormDatePicker
+        QFormFileUpload: typeof QFormFileUpload
+        QFormRadioGroup: typeof QFormRadioGroup
+        QFormRangeSlider: typeof QFormRangeSlider
+        QFormSelect: typeof QFormSelect
+        QFormSwitch: typeof QFormSwitch
+        QFormTable: typeof QFormTable
+        QFormText: typeof QFormText
+        QFormTextarea: typeof QFormTextarea
+        QSearch: typeof QSearch
+        QCard: typeof QCard
+        QCollapse: typeof QCollapse
+        QCollapseItem: typeof QCollapseItem
+        QCollapsibleSection: typeof QCollapsibleSection
+        QDialog: typeof QDialog
+        QDrawer: typeof QDrawer
+        QScrollContainer: typeof QScrollContainer
+        QTree: typeof QTree
+        QTreeNode: typeof QTreeNode
+        QNavSection: typeof QNavSection
+        QTab: typeof QTab
+        QConditionSelect: typeof QConditionSelect
+        QThemeToggle: typeof QThemeToggle
+    }
+
+    // 如果你未来扩展了全局属性，比如 $qyMessage
+    // export interface ComponentCustomProperties {
+    //   $qyMessage: ...
+    // }
+}
+
+// 同时导出插件类型，方便类型推导
+declare const QyaniComponents: Plugin
+export default QyaniComponents
