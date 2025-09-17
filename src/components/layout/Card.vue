5dd328e8d17a6583f2e7269e09fd1bee3daa0c68
qianrenni <15133897+Qianrenni@user.noreply.gitee.com>
2025-09-18 01:29:57 +0800

    fix:card

--- src/components/layout/Card.vue
+++ src/components/layout/Card.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="card container-column "
-       :class="{
+  <div :class="{
         'card-animation': animation
        }"
-    >
+       class="card container-column "
+  >
 
     <div class="card-header container-space-between ">
       <slot name="header"></slot>
@@ -11,7 +11,7 @@
 
     <div class="card-body container-space-between ">
       <slot name="left"></slot>
-      <div class="container-flex-1" >
+      <div class="container-flex-1">
         <slot name="default"/>
       </div>
       <slot name="right"></slot>
@@ -24,8 +24,8 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import { defineOptions } from 'vue'
+<script lang="ts" setup>
+import {defineOptions} from 'vue'
 
 defineProps({
   animation: {
@@ -39,41 +39,4 @@ defineOptions({
 </script>
 
 <style scoped>
-.card {
-  background-color: var(--card-bg);
-  border-radius: 8px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.05);
-  transition: transform 0.2s;
-}
-
-.card-header {
-  flex: 0 0 auto;
-}
-
-.card-body {
-  flex: 1 1 auto;
-}
-
-.card-footer {
-  flex: 0 0 auto;
-}
-
-.card-animation:hover {
-  /*修改转动点为中心*/
-  animation-name: up-down;
-  animation-duration: 1s;
-  animation-timing-function: ease-in-out;
-}
-
-@keyframes up-down {
-  0% {
-    transform: translateY(0) ;
-  }
-  50% {
-    transform: translateY(-10px) rotateY(270deg);
-  }
-  100% {
-    transform: translateY(0)  rotateY(360deg);
-  }
-}
 </style>
